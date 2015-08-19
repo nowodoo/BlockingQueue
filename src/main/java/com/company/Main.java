35c20a85f639f91843076b750c5254e44a267963
Jedaka <Jedaka@bk.ru>
2015-08-19 16:03:07 +0300

    third  commit

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -3,7 +3,36 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("nice!");
+
+        final BlockingQueue<String> queue = new BlockingQueue<String>(5);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 2; i++) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Element removed: " + queue.remove());
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 2; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    queue.add(String.valueOf(i));
+                    System.out.println("Element added");
+                }
+            }
+        }).start();
     }
 }
